9f777c215729cbe67c438af0944655e8a65d69ac
Mareta <maretahandanari@gmail.com>
2023-01-14 21:43:15 +0700

    Add animations

--- views/js.js
+++ views/js.js
@@ -55,4 +55,18 @@
     //   content.style.display = "block";
     // }
 
-   
+  //  add Animation 
+const boxes = document.querySelectorAll('.box')
+window.addEventListener('scroll', checkBoxes)
+checkBoxes()
+function checkBoxes(){
+    const triggerBottom = window.innerHeight/5*4
+    boxes.forEach(box =>{
+        const boxTop = box.getBoundingClientRect().top
+        if (boxTop < triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
+}
